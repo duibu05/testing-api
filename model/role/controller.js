69da9b228b275bd4d96e515381af2f7b78fd2ce8
卢纪霖 <duibu05@126.com>
2017-12-26 01:09:46 +0800

    fixed rename role bug

--- model/role/controller.js
+++ model/role/controller.js
@@ -6,11 +6,11 @@ class RoleController extends Controller {
   update(req, res, next) {
     this.facade.findById(req.params.id).then(oldRole => {
       if(oldRole){
-        userFacade.update({ 'role.name': oldRole.name }, { $set: { 'role.name': req.body.name } });
         this.facade.update({ _id: req.params.id }, req.body)
           .then((results) => {
             if (results.n < 1) { return res.sendStatus(404); }
             if (results.nModified < 1) { return res.sendStatus(304); }
+            userFacade.update({ 'role.name': oldRole.name }, { $set: { 'role.name': req.body.name } });
             res.json({code: 0, data: results});
           })
           .catch(err => next(err));
