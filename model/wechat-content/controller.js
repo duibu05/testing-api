581b2eb4049b117ebca6067bbadca66a1826dbe9
卢纪霖 <lujilin@guazi.com>
2018-02-24 13:19:21 +0800

    created at

--- model/wechat-content/controller.js
+++ model/wechat-content/controller.js
@@ -17,7 +17,7 @@ class WechatContentController extends Controller {
                 .then(collection => {
                   if (req.body.original && req.body.original === 'mapp') {
                     for(let i = 0, len = collection.length; i < len; i++) {
-                      collection[i].content = collection[i].content.replace(/<[^>]+>/g,"").substr(0,15);
+                      // collection[i].content = collection[i].content.replace(/<[^>]+>/g,"").substr(0,15);
                       collection[i].createdAt = '123'
                     }
                   }
