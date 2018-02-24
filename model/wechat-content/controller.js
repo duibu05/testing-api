aaaff768506ff890655281bbbf9e0063d0825b2c
卢纪霖 <lujilin@guazi.com>
2018-02-24 13:10:22 +0800

    improve wechat content created at date

--- model/wechat-content/controller.js
+++ model/wechat-content/controller.js
@@ -18,7 +18,7 @@ class WechatContentController extends Controller {
                   if (req.body.original && req.body.original === 'mapp') {
                     for(let i = 0, len = collection.length; i < len; i++) {
                       collection[i].content = collection[i].content.replace(/<[^>]+>/g,"").substr(0,15)
-                      collection[i].createdAt = moment(collection[i].createdAt).format('YYYY/MM/DD')
+                      collection[i].createdAtDate = moment(collection[i].createdAt).format('YYYY/MM/DD')
                     }
                   }
                   res.status(200).json({
