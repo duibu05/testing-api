127e72375ac61e38cf8ff9b01a50bf7c1c10a69f
卢纪霖 <duibu05@126.com>
2018-02-25 15:33:37 +0800

    upload question done

--- model/question/controller.js
+++ model/question/controller.js
@@ -1,6 +1,25 @@
 const Controller = require('../../lib/controller');
 const questionFacade = require('./facade');
+const categoryFacade = require('../category/facade')
 
-class QuestionController extends Controller {}
+class QuestionController extends Controller {
+  upload(req, res, next) {
+    Promise.all([
+      categoryFacade.findOne({ type: 'shiti', name: req.body.firstCat.name, level: 'first' }),
+      categoryFacade.findOne({ type: 'shiti', name: req.body.secondCat.name, level: 'second' })
+    ]).then(([cf, cs]) => {
+      req.body.firstCat.id = cf._id
+      req.body.secondCat.id = cs._id
+      this.facade.create(req.body).then(result => {
+        res.json({
+          code: 0, 
+          msg: 'ok',
+          data: result
+        })
+      })
+    })
+    .catch(e => next(e))
+  }
+}
 
 module.exports = new QuestionController(questionFacade);
