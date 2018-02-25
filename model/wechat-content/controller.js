ba7c1b0ff2ee3686caa0acdd1673995805b1fe9d
卢纪霖 <duibu05@126.com>
2018-02-25 09:58:36 +0800

    fixed admin system wechat content filter bug

--- model/wechat-content/controller.js
+++ model/wechat-content/controller.js
@@ -1,24 +1,23 @@
 const Controller = require('../../lib/controller');
 const wechatContentFacade = require('./facade');
 const moment = require('moment');
+const _ = require('lodash');
 
 class WechatContentController extends Controller {
     find(req, res, next) {
-        const countCondition = {
-          page: req.body.page,
-          limit: req.body.limit || 20,
-        };
-
-        req.body.cat && (countCondition.cat = req.body.cat)
+        const countCondition = {};
+        _.assign(countCondition, req.body, req.params, req.query)
+        if (!countCondition.limit) {
+          countCondition.limit = 20
+        }
 
         this.facade.count(countCondition).then(rows => {
             if (rows > 0) {
-              const queryCondition = {
-                page: req.body.page,
-                limit: req.body.limit || 20,
-              };
-      
-              req.body.cat && (queryCondition.cat = req.body.cat)
+              const queryCondition = {};
+              _.assign(queryCondition, req.body, req.params, req.query)
+              if (!queryCondition.limit) {
+                queryCondition.limit = 20
+              }
 
               const pageCount = Math.ceil(rows / +queryCondition.limit)
               this.facade.find(queryCondition)
