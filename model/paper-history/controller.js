949f239be4632e7b6b3a4dfaaa4865dd6518781c
卢纪霖 <lujilin@guazi.com>
2018-02-26 13:56:04 +0800

    fixed history is null bug

--- model/paper-history/controller.js
+++ model/paper-history/controller.js
@@ -7,6 +7,12 @@ class PaperHistoryController extends Controller {
     Promise.all([this.facade.findById(req.body.id), historyFacade.findOne({ openId: req.body.openId })])
       .then(([doc, history]) => {
         if (!doc) { return res.sendStatus(404); }
+        if (!history) {
+          history = {
+            questionSize: 0,
+            correctRate: "0%"
+          }
+        }
         return res.status(200).json({
             code:0,
             data: {
