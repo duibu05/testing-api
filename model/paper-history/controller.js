57284703cc3001267b8130e9d85c8be418e7558f
卢纪霖 <lujilin@guazi.com>
2018-02-26 13:49:12 +0800

    fixed history details bug

--- model/paper-history/controller.js
+++ model/paper-history/controller.js
@@ -1,10 +1,10 @@
 const Controller = require('../../lib/controller');
 const paperHistoryFacade = require('./facade');
-const history = require('../history/facade');
+const historyFacade = require('../history/facade');
 
 class PaperHistoryController extends Controller {
   findById(req, res, next) {
-    Promise.all([this.facade.findById(req.body.id), history.facade.findOne({ openId: req.body.openId })])
+    Promise.all([this.facade.findById(req.body.id), historyFacade.findOne({ openId: req.body.openId })])
       .then(([doc, history]) => {
         if (!doc) { return res.sendStatus(404); }
         return res.status(200).json({
