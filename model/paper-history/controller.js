b964896dd4e45ebb0fdbe25d121017fdc9e093d6
卢纪霖 <lujilin@guazi.com>
2018-02-28 11:59:07 +0800

    paper history statistics update

--- model/paper-history/controller.js
+++ model/paper-history/controller.js
@@ -1,17 +1,22 @@
 const Controller = require('../../lib/controller');
 const paperHistoryFacade = require('./facade');
 const historyFacade = require('../history/facade');
+const paperFacade = require('../paper/facade');
 
 class PaperHistoryController extends Controller {
   findById(req, res, next) {
-    Promise.all([this.facade.findById(req.body.id), historyFacade.findOne({ openId: req.body.openId })])
-      .then(([doc, history]) => {
+    this.facade.findById(req.body.id)
+      .then((doc) => {
         if (!doc) { return res.sendStatus(404); }
-        if (!history) {
-          history = {
-            questionSize: 0,
-            correctRate: "0%"
+        const right = doc.questionsHistory.filter(v => {
+          if (v.userAnswer.sort().join(',') === v.rightAnswer.sort().join(',')) {
+            return true
           }
+          return false
+        }).length
+        const history = {
+          questionSize: doc.questionsHistory.length,
+          correctRate: Math.round(right / doc.questionsHistory.length * 100) + '%'
         }
         return res.status(200).json({
             code:0,
