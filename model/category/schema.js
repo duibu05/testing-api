3960f1242c98f1b8c888613bbfed100d11f4d05f
卢纪霖 <duibu05@126.com>
2018-03-17 19:35:36 +0800

    update category schema

--- model/category/schema.js
+++ model/category/schema.js
@@ -1,11 +1,19 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+const catSchema = new Schema({
+  id: String,
+  name: String
+}, { _id: false });
 
 const categorySchema = new Schema({
   name: { type: String, required: true },
   type: { type: String, required: true },
   level: { type: String, required: true, default: 'first' },
+  first: { type: catSchema, required: false },
+  second: { type: catSchema, required: false },
+  third: { type: catSchema, required: false },
+  forth: { type: catSchema, required: false },
   image: { type: String, required: true }
 });
 
